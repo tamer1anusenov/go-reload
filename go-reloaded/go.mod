1e922cf3a0a3b4691d11a8f0a21a74b21653bbec
tamer1anusenov <130888410+tamer1anusenov@users.noreply.github.com>
2025-06-07 15:00:11 +0500

    Add files via upload

---
+++ go-reloaded/go.mod
@@ -0,0 +1,5 @@
+module go-reloaded
+
+go 1.24.2
+
+require golang.org/x/text v0.25.0
---
+++ go-reloaded/main.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"fmt"
+	"go-reloaded/processor"
+	"os"
+)
+
+func main() {
+	// Check command line arguments
+	if len(os.Args) != 3 {
+		fmt.Fprintf(os.Stderr, "Usage: %s <input_file> <output_file>\n", os.Args[0])
+		os.Exit(1)
+	}
+
+	inputFile := os.Args[1]
+	outputFile := os.Args[2]
+
+	// Read input file
+	content, err := os.ReadFile(inputFile)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Error reading input file: %v\n", err)
+		os.Exit(1)
+	}
+
+	// Process the content using the processor package
+	processedText := processor.ProcessText(string(content))
+
+	// Write to output file
+	err = os.WriteFile(outputFile, []byte(processedText), 0644)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Error writing output file: %v\n", err)
+		os.Exit(1)
+	}
+}
